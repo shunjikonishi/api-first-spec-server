aacf4685058ca6f6cf6ed4ac2809c24beae1d1c8
shunjikonishi <swandouble@gmail.com>
2018-05-27 23:59:37 +0900

    Add ejs

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,14 @@ import express  from "express";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.set("views", "server/views");
+app.set("view engine", "ejs");
+
+app.get("/*", (req, res) => {
+  res.render("index", {
+    title: "test"
+  });
+});
+
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));

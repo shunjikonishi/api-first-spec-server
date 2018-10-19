f1a3ce6a35de2ffd0c1ca6853f6fb253bd090ad1
shunjikonishi <swandouble@gmail.com>
2018-10-20 08:09:59 +0900

    Remove all frontend related code

--- frontend/Messages.elm
+++
@@ -1,9 +0,0 @@
-module Messages exposing (..)
-
-import Models.SpecList as SpecList
-import Models.SpecDetail as SpecDetail
-
-
-type Msg
-    = SpecListMsg SpecList.Msg
-    | SpecDetailMsg SpecDetail.Msg
--- frontend/entry.js
+++
@@ -1,7 +0,0 @@
-const Elm = require("./Main.elm");
-require("./main.scss");
-
-document.addEventListener("DOMContentLoaded", () => {
-  const mainDiv = document.getElementById("main");
-  Elm.Main.embed(mainDiv);
-});
